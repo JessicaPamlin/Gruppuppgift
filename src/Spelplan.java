19efe75ac1bd883a91400e833e177149f3479f16
jessica pamlin <jessicapamlins@hotmail.se>
2021-10-28 09:33:30 +0200

    gjort spelplan

---
+++ src/Spelplan.java
@@ -0,0 +1,27 @@
+
+public class Spelplan {
+	static String[] spelplan = new String[9];
+
+	public static void skrivUtSpelplanen() { //skriver ut spelplanen
+
+		System.out.println("\t| " + spelplan[0] + " | " + spelplan[1] + " | " + spelplan[2] + " |");
+		System.out.println("\t|-----------|");
+		System.out.println("\t| " + spelplan[3] + " | " + spelplan[4] + " | " + spelplan[5] + " |");
+		System.out.println("\t|-----------|");
+		System.out.println("\t| " + spelplan[6] + " | " + spelplan[7] + " | " + spelplan[8] + " |");
+
+
+		System.out.println("  *************************");
+
+
+	}
+
+	public static void fyllSpelplan() { //hur stor spelplanen skall vara
+		for (int position = 0; position < 9; position++) {
+			spelplan[position] = String.valueOf(position+1);
+		}
+	}
+
+}
+
+
---
+++ src/TicTacToe.java
@@ -0,0 +1,13 @@
+
+public class TicTacToe {
+
+	public static void main (String[] args) {
+		System.out.println("  Välkommen till Tre i rad!");
+		System.out.println("  *************************");
+
+		Spelplan.fyllSpelplan();
+		Spelplan.skrivUtSpelplanen();
+
+	}
+
+}
